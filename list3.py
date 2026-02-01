9c7b1c0f265e918e002c53751240770eb98563be
kifayat96 <kifayatullah9611@gmail.com>
2026-02-01 15:13:06 +0500

    my code

---
+++ list3.py
@@ -0,0 +1,8 @@
+my_list = ["sleep", "eat", "play", "code"]
+print("Original list:", my_list)
+my_list.remove("eat")
+print("Updated list after removing 'eat':", my_list)
+my_list.pop(2)
+print("List after popping index 2:", my_list) 
+my_list[1] = "work"
+print("Final updated list:", my_list)
