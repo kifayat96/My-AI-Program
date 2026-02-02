5f274eb3dea8c50722aaa951d5526057cf80dfb3
kifayat96 <kifayatullah9611@gmail.com>
2026-02-02 23:31:25 +0500

    my code

---
+++ total_sum_of_a_summing.py
@@ -0,0 +1,9 @@
+pricess=[100,200,300,400,500]
+total_sum=0
+for price in pricess:
+    total_sum+=price
+print("Total sum of prices:",total_sum)
+
+#easy is
+prices = [100, 200, 300, 400, 500]
+print("Total sum of prices:", sum(prices))
