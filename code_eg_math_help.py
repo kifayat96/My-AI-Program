e18b8869a980c7139206a9725d70af83039e2803
kifayat96 <kifayatullah9611@gmail.com>
2026-02-02 23:57:47 +0500

    math

---
+++ code_eg_math_help.py
@@ -0,0 +1,12 @@
+# --- Code Example 1: Math Helpers ---
+
+numbers = [10, 50, 5, 100, 25]
+
+print("Total items:", len(numbers))   
+print("Largest number:", max(numbers)) 
+print("Smallest number:", min(numbers)) 
+print("Sum of all numbers:", sum(numbers)) 
+
+# Calculate Average (Sum / Count)
+average = sum(numbers) / len(numbers)
+print("Average value:", average)       
