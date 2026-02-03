eb392b29b6f91f49edbd9631d7f76d11185bd3fc
kifayat96 <kifayatullah9611@gmail.com>
2026-02-03 23:57:37 +0500

    Practice Tuples Sets and Dicts

---
+++ tuple5.py
@@ -0,0 +1,11 @@
+# Code: Tuple Unpacking
+# Maan lijiye AI ne ek photo dekhi aur bataya ye kya hai aur kitne % sure hai
+ai_result = ("Billi", 95.5)
+
+# Ye hai unpacking - Ek line mein dono values alag kar li
+jaanwar, yakeen = ai_result
+
+print(f"AI ne pehchana: {jaanwar}")
+print(f"AI ka yakeen (Confidence): {yakeen}%")
+
+# Task: Upar "Billi" ko "Insaan" aur 95.5 ko 99.9 karke run karein.
---
+++ tuupl6.py
@@ -0,0 +1,13 @@
+# Code 1: RGB Colors (Tuple)
+# Red, Green, Blue colors ke code fix hote hain
+
+red_color = (255, 0, 0)
+green_color = (0, 255, 0)
+blue_color = (0, 0, 255)
+
+print(f"Red ka code hai: {red_color}")
+print(f"Green ka code hai: {green_color}")
+
+# Task: Niche Black color ka code khud likhein (0, 0, 0) aur print karein
+# black_color = (0, 0, 0)
+# print(black_color)
