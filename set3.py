b15825964382610426c1a6d0f54f81b015d80e32
kifayat96 <kifayatullah9611@gmail.com>
2026-02-05 23:52:15 +0500

    my code

---
+++ set3.py
@@ -0,0 +1,10 @@
+# Code 3: Missing Skills
+my_skills = {"Python", "AI"}
+job_requirements = {"AI", "Python", "Java", "SQL"}
+
+# Difference: Jo Job ko chahiye par mere paas nahi
+missing_skills = job_requirements.difference(my_skills)
+
+print(f"Mujhe ye seekhna padega: {missing_skills}")
+
+# Task: 'my_skills' mein "SQL" daal kar run karein.
