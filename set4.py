97903fc46159967eab51578fc22ad741ef24da93
kifayat96 <kifayatullah9611@gmail.com>
2026-02-05 23:54:11 +0500

    new code

---
+++ set4.py
@@ -0,0 +1,14 @@
+# Code 4: Party Guest List
+guests = {"Ali", "Hamza", "Bilal"}
+
+print(f"Original List: {guests}")
+
+# Naya mehman aaya
+guests.add("Inayatullah")
+
+# Ek mehman chala gaya
+guests.remove("Ali")
+
+print(f"Final Guest List: {guests}")
+
+# Task: "Inayatullah" ki jagah apne dost ka naam add karein.
