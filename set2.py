4b3890f91b621b0041901d07e0a79a655901c71c
kifayat96 <kifayatullah9611@gmail.com>
2026-02-05 23:50:27 +0500

    my code

---
+++ set2.py
@@ -0,0 +1,9 @@
+# Code 2: Common Skills Finder
+my_skills = {"Python", "AI", "C++", "HTML"}
+job_requirements = {"AI", "Python", "Java", "SQL"}
+
+# Intersection: Jo dono mein common hai
+common = my_skills.intersection(job_requirements)
+print(f"Skills jo match ho gayi: {common}")
+
+# Task: 'my_skills' mein "Java" add karein aur dekhein result badalta hai ya nahi.
