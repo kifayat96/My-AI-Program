7d7a93d32f37a97a4a2e89dd700cefbc6df84286
kifayat96 <kifayatullah9611@gmail.com>
2026-02-05 23:49:08 +0500

    my code

---
+++ sets.py
@@ -0,0 +1,9 @@
+# Code 1: Email Cleaner
+# Notice karein 'ali@gmail.com' do baar likha hai
+emails = {"ali@gmail.com", "sara@yahoo.com", "ali@gmail.com", "khan@hotmail.com"}
+
+print(f"Total Emails: {len(emails)}")
+print(f"Unique Emails: {emails}")
+
+# Task: Ismein apni email ID add karke run karein.
+# emails.add("meri_email@gmail.com")
