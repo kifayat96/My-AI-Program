2fc4bd876a50f1a630c16edceb73a7eb1481cdfa
kifayat96 <kifayatullah9611@gmail.com>
2026-02-06 23:03:02 +0500

    new code

---
+++ dictionary3.py
@@ -0,0 +1,18 @@
+# Code 3: Updating Scores
+cricket_score = {
+    "Babar Azam": 50,
+    "Rizwan": 45,
+    "Shaheen": 10
+}
+
+print(f"Old Scores: {cricket_score}")
+
+# Babar ne century maari (Update)
+cricket_score["Babar Azam"] = 100
+
+# Naya player aaya (Add)
+cricket_score["Fakhar"] = 60
+
+print(f"New Scores: {cricket_score}")
+
+# Task: Rizwan ka score 0 kar dein aur run karein.
