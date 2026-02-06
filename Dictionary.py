fdee9b728b94d214eb3f7293aa051901ee16a1ae
kifayat96 <kifayatullah9611@gmail.com>
2026-02-06 22:58:12 +0500

    new

---
+++ Dictionary.py
@@ -0,0 +1,14 @@
+# Code 1: Student Data
+student = {
+    "name": "Kifayat",
+    "city": "Buner",
+    "course": "AI",
+    "semester": 6
+}
+
+print(f"Student Name: {student['name']}")
+print(f"City: {student['city']}")
+
+# Task: Isme "country": "Pakistan" add karke run karein.
+student["country"] = "Pakistan"
+print(student)
