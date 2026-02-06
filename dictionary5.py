ead38cae45ff3fd3e3354aea9978b3e53c465321
kifayat96 <kifayatullah9611@gmail.com>
2026-02-06 23:07:57 +0500

    my new code

---
+++ dictionary5.py
@@ -0,0 +1,16 @@
+# Code 5: Nested Structure (Complex Data)
+my_class = {
+    "student_1": {
+        "name": "Ali",
+        "marks": 90
+    },
+    "student_2": {
+        "name": "Sara",
+        "marks": 95
+    }
+}
+
+# Sirf Sara ke marks access karna
+print(f"Sara ke marks: {my_class['student_2']['marks']}")
+
+# Task: Ek naya 'student_3' add karein is dictionary mein.
