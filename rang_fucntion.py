80054a231e61e5be6b5b25174165ca770996e3bf
kifayat96 <kifayatullah9611@gmail.com>
2026-02-07 19:18:26 +0500

    my code

---
+++ rang_fucntion.py
@@ -0,0 +1,9 @@
+# Code 1: Simple Counter
+# 1 se lekar 10 tak ginti print karega (11 count nahi hoga)
+
+print("Starting Count:")
+for number in range(1, 11):
+    print(f"Number: {number}")
+
+# Task: range(1, 11) ko change karke range(10, 21) karein.
+# Isse 10 se 20 tak print hoga.
