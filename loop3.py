9254b3a4735233f9bc4aa7dcb2650c6748cf7114
kifayat96 <kifayatullah9611@gmail.com>
2026-02-07 19:24:40 +0500

    code m

---
+++ loop3.py
@@ -0,0 +1,13 @@
+# Code 5: Coordinate Generator
+# x aur y coordinates banana
+
+x_values = [1, 2]
+y_values = [10, 20]
+
+for x in x_values:
+    for y in y_values:
+        print(f"Coordinate: ({x}, {y})")
+
+# Output aayega: (1,10), (1,20), (2,10), (2,20)
+
+# Task: x_values mein 3 add karein.
