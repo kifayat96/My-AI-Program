b604512d6ca8c0582769d413c615366e9d5721a3
kifayat96 <kifayatullah9611@gmail.com>
2026-02-07 19:20:27 +0500

    code

---
+++ fuction.py
@@ -0,0 +1,10 @@
+# Code 2: AI Models List
+models = ["ChatGPT", "Gemini", "DeepSeek", "Llama"]
+
+print("Available AI Models:")
+# Har model ke liye loop chalega
+for model in models:
+    print(f"Analyzing: {model}")
+
+# Task: List mein "Claude" add karein aur dobara run karein.
+# models.append("Claude")
