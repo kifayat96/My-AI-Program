454ef45d7e649773094f37321503f15352b4358b
kifayat96 <kifayatullah9611@gmail.com>
2026-02-08 22:38:31 +0500

    my code

---
+++ simple_fuction.py
@@ -0,0 +1,10 @@
+# Code 1: Greet Function
+def greet_user():
+    print("Assalam-o-Alaikum!")
+    print("Welcome to AI Course.")
+
+# Ab function ko call karte hain
+greet_user()
+
+# Task: Is function ke andar ek aur line add karein: print("Happy Coding!")
+# Phir run karein.
