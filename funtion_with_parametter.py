f0280cf922f14763c5b04ef427790cfc61676ff8
kifayat96 <kifayatullah9611@gmail.com>
2026-02-08 22:41:24 +0500

    code m

---
+++ funtion_with_parametter.py
@@ -0,0 +1,11 @@
+# Code 2: Personalized Greeting
+def greet_name(name):
+    print(f"Hello, {name}!")
+    print("Kaise hain aap?")
+
+# Alag alag naam ke saath call karein
+greet_name("Ali")
+greet_name("Hamza")
+
+# Task: Apna naam likh kar function call karein.
+# greet_name("Mera Naam")
