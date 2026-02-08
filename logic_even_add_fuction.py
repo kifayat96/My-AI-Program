25fd7c5c7ae475accd21e48f32b6371278f29618
kifayat96 <kifayatullah9611@gmail.com>
2026-02-08 22:47:59 +0500

    code of night

---
+++ logic_even_add_fuction.py
@@ -0,0 +1,12 @@
+# Code 5: Even Odd Checker
+def check_number(num):
+    if num % 2 == 0:
+        return "Even (Juft)"
+    else:
+        return "Odd (Taaq)"
+
+# Check karte hain
+print(f"7 is: {check_number(7)}")
+print(f"10 is: {check_number(10)}")
+
+# Task: check_number(25) try karein.
