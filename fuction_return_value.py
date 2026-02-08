e9aea5c4804ff8c441979a5b10750c025bef692a
kifayat96 <kifayatullah9611@gmail.com>
2026-02-08 22:43:27 +0500

    my code

---
+++ fuction_return_value.py
@@ -0,0 +1,10 @@
+# Code 3: Square Function (Maths)
+def square_number(number):
+    result = number * number
+    return result  # Jawab wapis bhejo
+
+# Jawab ko variable mein save karein
+answer = square_number(5)
+print(f"5 ka square hai: {answer}")
+
+# Task: square_number(10) ka result print karein.
