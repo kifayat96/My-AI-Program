9ba9f038cc65e828472edebbf48ce981c3341e21
kifayat96 <kifayatullah9611@gmail.com>
2026-02-16 23:26:27 +0500

    my code

---
+++ selectionSort.py
@@ -0,0 +1,10 @@
+def selection_sort(nums):
+    for i in range(len(nums)):
+        min_index = i
+        for j in range(i+1, len(nums)):
+            if nums[j] < nums[min_index]:
+                min_index = j
+        nums[i], nums[min_index] = nums[min_index], nums[i]
+    return nums
+
+print(selection_sort([64, 25, 12, 22, 11]))
