7fde595276ac115c4caf27e95b2add1957cb0dc3
kifayat96 <kifayatullah9611@gmail.com>
2026-02-17 23:36:10 +0500

    new code

---
+++ insertion_sort.py
@@ -0,0 +1,11 @@
+def insertion_sort(nums):
+    for i in range(1, len(nums)):
+        key = nums[i]
+        j = i - 1
+        while j >= 0 and nums[j] > key:
+            nums[j + 1] = nums[j]
+            j -= 1
+        nums[j + 1] = key
+    return nums
+
+print(insertion_sort([9, 5, 1, 4, 3]))
