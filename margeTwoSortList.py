1ae24943c14f3e33244cd0211a2ea97b7c616770
kifayat96 <kifayatullah9611@gmail.com>
2026-02-19 23:43:39 +0500

    my new code

---
+++ margeTwoSortList.py
@@ -0,0 +1,19 @@
+def merge_sorted(a, b):
+    i = j = 0
+    result = []
+    while i < len(a) and j < len(b):
+        if a[i] < b[j]:
+            result.append(a[i])
+            i += 1
+        else:
+            result.append(b[j])
+            j += 1
+    while i < len(a):
+        result.append(a[i])
+        i += 1
+    while j < len(b):
+        result.append(b[j])
+        j += 1
+    return result
+
+print(merge_sorted([1, 3, 5], [2, 4, 6]))
