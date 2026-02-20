8152eb215c574c502e0dc2ba1f2b1a497787b5e3
kifayat96 <kifayatullah9611@gmail.com>
2026-02-20 23:55:01 +0500

    my n code

---
+++ cheakTheBalanceParantheses.py
@@ -0,0 +1,12 @@
+def balanced(s):
+    stack = []
+    for ch in s:
+        if ch == "(":
+            stack.append(ch)
+        elif ch == ")":
+            if not stack:
+                return False
+            stack.pop()
+    return len(stack) == 0
+
+print(balanced("(())()"))
