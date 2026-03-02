a3a4712f345b935301eb9850aaab923e88f1339f
kifayat96 <kifayatullah9611@gmail.com>
2026-03-02 23:37:12 +0500

    my code

---
+++ basicStateUsinglist.py
@@ -0,0 +1,11 @@
+def stack_example():
+    stack = []
+    
+    stack.append(10)   # push
+    stack.append(20)
+    stack.append(30)
+
+    print(stack.pop()) # pop
+    print(stack)
+
+stack_example()
