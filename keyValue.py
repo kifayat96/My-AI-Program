9465a603d4189267dc10c3859569c06853ed11dd
kifayat96 <kifayatullah9611@gmail.com>
2026-03-02 23:33:36 +0500

    my code

---
+++ keyValue.py
@@ -0,0 +1,4 @@
+def profile(name,age,city):
+    return f"i am {name} and i am {age} year old i am from {city} "
+user=profile(age=25,city="buner",name="kifayat")
+print(user)
---
+++ sraricLenOfString.py
@@ -0,0 +1,4 @@
+def concatenate_strings(*args):
+    return " ".join(args)
+result = concatenate_strings("Hello", "world", "from", "Python!")
+print(result)
