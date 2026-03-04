eb38d507c0105065dd4757f5d031bf521774f347
kifayat96 <kifayatullah9611@gmail.com>
2026-03-04 23:53:22 +0500

    my code

---
+++ reverseStrUsingStake.py
@@ -0,0 +1,11 @@
+def reverse_string(s):
+    stack = []
+    for ch in s:
+        stack.append(ch)
+
+    reversed_str = ""
+    while stack:
+        reversed_str += stack.pop()
+    return reversed_str
+
+print(reverse_string("Artificial"))
