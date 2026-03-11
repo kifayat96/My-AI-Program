beca7c9508c66c94807d731aae78a8d9942f12d4
kifayat96 <kifayatullah9611@gmail.com>
2026-03-12 00:13:29 +0500

    my code

---
+++ insertAtEnd.py
@@ -0,0 +1,13 @@
+def insert_end(self, data):
+    new_node = Node(data)
+
+    if self.head is None:
+        self.head = new_node
+        return
+
+    temp = self.head
+    while temp.next:
+        temp = temp.next
+
+    temp.next = new_node
+    new_node.prev = temp
