eb26acd83fb11a4a043b99d0d1aa696249a285f7
kifayat96 <kifayatullah9611@gmail.com>
2026-03-24 00:33:06 +0500

    my code

---
+++ removeDublicatFormList.py
@@ -0,0 +1,10 @@
+def remove_duplicates(nums):
+    result = []
+    
+    for n in nums:
+        if n not in result:
+            result.append(n)
+    
+    return result
+
+print(remove_duplicates([1,2,2,3,4,4]))
